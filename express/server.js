8a82a25f8544cbab570b6ae1a33c72ca8ef250c6
Kamyab <karouhifar@gmail.com>
2021-04-26 15:13:04 -0400

    serveless HTTP

---
+++ express/server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const path = require('path');
+const serverless = require('serverless-http');
+const app = express();
+const bodyParser = require('body-parser');
+
+const router = express.Router();
+router.get('/', (req, res) => {
+  // res.writeHead(200, { 'Content-Type': 'text/html' });
+  res.sendFile(path.join(__dirname, '../index.html'));
+  // res.end();
+});
+router.get('/another', (req, res) => res.json({ route: req.originalUrl }));
+router.post('/', (req, res) => res.json({ postBody: req.body }));
+
+app.use(express.json());
+app.use('/.netlify/functions/server', router);  // path must route to lambda
+// app.use('/', (req, res) => res.sendFile(path.join(__dirname, '../index.html')));
+
+module.exports = app;
+module.exports.handler = serverless(app);
