66c888e8d358177402d5dfe3cf6c2a351e1b7797
Kamyab <karouhifar@gmail.com>
2021-04-26 20:49:27 -0400

    serveless HTTP

--- express/server.js
+++ express/server.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const path = require('path');
+const cors = require('cors');
 const serverless = require('serverless-http');
 const app = express();
 const router = express.Router();
 
 app.use(express.json());
-
+ // Cross-Origin Resource Sharing
+ app.use(cors());
 router.get('/', (req, res) => {
   // res.sendFile(path.join(__dirname, './dist/index.html'));
 });
