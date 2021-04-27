9401283a6fe8fcf61a201ead7c19ff65b40a6c96
Kamyab <karouhifar@gmail.com>
2021-04-26 20:40:09 -0400

    serveless HTTP

--- server-local.js
+++
@@ -1,3 +0,0 @@
-const app = require('./express/server');
-
-app.listen(9000, () => console.log('Local app listening on port 9000!'));
--- server.js
+++ server.js
@@ -3,16 +3,20 @@ const path = require('path');
 const serverless = require('serverless-http');
 const app = express();
 const router = express.Router();
+const HTTP_PORT = process.env.PORT || 9000;
 app.use(express.json());
-app.use('/.netlify/functions/server', router); 
+
 router.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '../index.html'));
+  res.sendFile(path.join(__dirname, './dist/index.html'));
 });
 router.get('/another', (req, res) => res.json({ route: req.originalUrl }));
 router.post('/', (req, res) => res.json({ postBody: req.body }));
 
+app.use('/.netlify/functions/server', router);  // path must route to lambda
+app.use('/', (req, res) => res.sendFile(path.join(__dirname, './dist/index.html')));
 
+app.listen(HTTP_PORT,  () => {
+  console.log(`CORS-enabled web server listening on port ${HTTP_PORT}`)
+});
 
-
-module.exports = app;
 module.exports.handler = serverless(app);
