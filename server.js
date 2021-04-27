76d4d8e1f8bf1b3575245032fb7153c36f6e0247
Kamyab <karouhifar@gmail.com>
2021-04-26 20:43:11 -0400

    serveless HTTP

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const HTTP_PORT = process.env.PORT || 9000;
 app.use(express.json());
 
 router.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, './dist/index.html'));
+  // res.sendFile(path.join(__dirname, './dist/index.html'));
 });
 router.get('/another', (req, res) => res.json({ route: req.originalUrl }));
 router.post('/', (req, res) => res.json({ postBody: req.body }));
