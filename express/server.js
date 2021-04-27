a28124df3c363c80833e2ef8a772b90c395da2b6
Kamyab <karouhifar@gmail.com>
2021-04-26 20:47:31 -0400

    serveless HTTP

--- express/server.js
+++ express/server.js
@@ -3,7 +3,7 @@ const path = require('path');
 const serverless = require('serverless-http');
 const app = express();
 const router = express.Router();
-const HTTP_PORT = process.env.PORT || 9000;
+
 app.use(express.json());
 
 router.get('/', (req, res) => {
@@ -13,10 +13,11 @@ router.get('/another', (req, res) => res.json({ route: req.originalUrl }));
 router.post('/', (req, res) => res.json({ postBody: req.body }));
 
 app.use('/.netlify/functions/server', router);  // path must route to lambda
-app.use('/', (req, res) => res.sendFile(path.join(__dirname, './dist/index.html')));
+app.use('/', (req, res) => res.sendFile(path.join(__dirname, '../dist/index.html')));
 
-app.listen(HTTP_PORT,  () => {
-  console.log(`CORS-enabled web server listening on port ${HTTP_PORT}`)
-});
+// app.listen(HTTP_PORT,  () => {
+//   console.log(`CORS-enabled web server listening on port ${HTTP_PORT}`)
+// });
 
+module.exports = app;
 module.exports.handler = serverless(app);
