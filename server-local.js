a335f1207983003f585f7cb471b6c59516b368ee
Kamyab <karouhifar@gmail.com>
2021-04-26 20:51:21 -0400

    serveless HTTP

--- server-local.js
+++ server-local.js
@@ -1,3 +1,3 @@
 const app = require('./express/server');
-
-app.listen(9000, () => console.log('Local app listening on port 9000!'));
+const HTTP_PORT = process.env.PORT || 8080;
+app.listen(HTTP_PORT, () => console.log(`Local app listening on port ${HTTP_PORT}!`));
