4524465ce82264c772c6b4e4d3600e5d35cf9803
Kamyab <karouhifar@gmail.com>
2021-04-26 21:36:46 -0400

    serveless HTTP

--- express/server.js
+++ express/server.js
@@ -13,18 +13,18 @@ const router = express.Router();
 app.use(express.json());
  // Cross-Origin Resource Sharing
  app.use(cors());
-router.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, './dist/index.html'));
+ app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '../dist/index.html'));
 });
-router.get('/another', (req, res) => res.json({ route: req.originalUrl }));
-router.post('/', (req, res) => res.json({ postBody: req.body }));
+app.get('/another', (req, res) => res.json({ route: req.originalUrl }));
+app.post('/', (req, res) => res.json({ postBody: req.body }));
 
-app.use('/.netlify/functions/server', router);  // path must route to lambda
+// app.use('/.netlify/functions/server', router);  // path must route to lambda
 app.use('/', (req, res) => res.sendFile(path.join(__dirname, '../dist/index.html')));
 
 // app.listen(HTTP_PORT,  () => {
 //   console.log(`CORS-enabled web server listening on port ${HTTP_PORT}`)
 // });
 
-// module.exports = app;
+module.exports = app;
 module.exports.handler = serverless(app);
